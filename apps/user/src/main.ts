929cceb05371016d40822cec3f5c0ac959e5dab6
venkatesh <morlavenkatesh19@gmail.com>
2026-04-20 21:46:27 +0530

    user initial setup with graphql

--- apps/user/src/main.ts
+++ apps/user/src/main.ts
@@ -1,32 +1,36 @@
 import express from 'express';
-import { readFileSync } from 'node:fs';
+import { typeDefs } from './schema/user.schema';
 import { ApolloServer } from '@apollo/server';
 import { me } from './resolvers/me';
 import { expressMiddleware } from '@as-integrations/express5';
+import { buildSubgraphSchema } from '@apollo/subgraph';
 
-const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-const app = express();
+(async () => {
+  const app = express();
 
-const typeDefs = readFileSync('./schema/user.graphql', 'utf-8');
+  const apolloServer = new ApolloServer({
+    schema: buildSubgraphSchema([
+      {
+        typeDefs,
+        resolvers: {
+          Query: {
+            me: me!,
+          },
+        },
+      },
+    ]),
+  });
 
-const apolloServer = new ApolloServer({
-  typeDefs,
-  resolvers: {
-    Query: {
-      me,
-    },
-  },
-});
+  await apolloServer.start();
 
-apolloServer.start().then(() => {
   app.use('/graphql', express.json(), expressMiddleware(apolloServer));
   app.get('/', (req, res) => {
     res.send({ message: 'Hello API' });
   });
 
-  app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
+  app.listen(port, () => {
+    console.log(`[ ready ] http://localhost:${port}`);
   });
-});
+})();
